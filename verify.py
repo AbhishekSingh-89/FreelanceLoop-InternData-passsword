32d263f5f41ce3a5f08cff9d06ed66e9e2dca892
Abhishek Singh <ctfintellectsy2@gmail.com>
2025-12-25 00:03:54 +0530

    Update verify.py

--- verify.py
+++ verify.py
@@ -1,27 +1,7 @@
-import hashlib
+import base64
 
-EXPECTED_HASH = "e0c6830bae960a200289e1109ab2fae46b72ce2bb985dc965eb8907721ccf734"
+code = """
+aW1wb3J0IGhhc2hsaWIKCkVYUEVDVEVEX0hBU0ggPSAiZTBjNjgzMGJhZTk2MGEyMDAyODllMTEwOWFiMmZhZTQ2YjcyY2UyYmI5ODVkYzk2NWViODkwNzcyMWNjZjczNCIKClBBU1RFQklOX1VSTCA9ICJodHRwczovL3Bhc3RlYmluLmNvbS93UnduSEV3aiIKClBBU1RFQklOX1BBU1NXT1JEID0gIkNoZWNrSW5EQHJrV2ViIgoKZGVmIHZlcmlmeV9mbGFnKHVzZXJfaW5wdXQpOgogICAgcmV0dXJuIGhhc2hsaWIuc2hhMjU2KHVzZXJfaW5wdXQuZW5jb2RlKCkpLmhleGRpZ2VzdCgpID09IEVYUEVDVEVEX0hBU0gKCmRlZiBtYWluKCk6CiAgICBwcmludCgiRnJlZWxhbmNlIExvb3AgLSBJbnRlcm5hbCBWZXJpZmljYXRpb24gVG9vbCIpCiAgICBwcmludCgiPT09PT09PT09PT09PT09PT09PT09PT09PT09PT09XG4iKQoKICAgIHVzZXJfZmxhZyA9IGlucHV0KCJFbnRlciB2ZXJpZmljYXRpb24gZmxhZzogIikuc3RyaXAoKQoKICAgIGlmIHZlcmlmeV9mbGFnKHVzZXJfZmxhZyk6CiAgICAgICAgcHJpbnQoIlxuWytdIFZlcmlmaWNhdGlvbiBzdWNjZXNzZnVsLiIpCiAgICAgICAgcHJpbnQoIlBhc3RlYmluIFVSTDoiKQogICAgICAgIHByaW50KFBBU1RFQklOX1VSTCkKICAgICAgICBwcmludCgiXG5QYXN0ZWJpbiBQYXNzd29yZDoiKQogICAgICAgIHByaW50KFBBU1RFQklOX1BBU1NXT1JEKQogICAgZWxzZToKICAgICAgICBwcmludCgiXG5bLV0gSW52YWxpZCBmbGFnLiIpCgppZiBfX25hbWVfXyA9PSAiX19tYWluX18iOgogICAgbWFpbigp
+"""
 
-PASTEBIN_URL = "https://pastebin.com/wRwnHEwj"
-PASTEBIN_PASSWORD = "CheckInD@rkWeb"
-
-def verify_flag(user_input):
-    return hashlib.sha256(user_input.encode()).hexdigest() == EXPECTED_HASH
-
-def main():
-    print("Freelance Loop - Internal Verification Tool")
-    print("===========================\n")
-
-    user_flag = input("Enter verification flag: ").strip()
-
-    if verify_flag(user_flag):
-        print("\n[+] Verification successful.")
-        print("Pastebin URL:")
-        print(PASTEBIN_URL)
-        print("\nPastebin Password:")
-        print(PASTEBIN_PASSWORD)
-    else:
-        print("\n[-] Invalid flag.")
-
-if __name__ == "__main__":
-    main()
+exec(base64.b64decode(code))
